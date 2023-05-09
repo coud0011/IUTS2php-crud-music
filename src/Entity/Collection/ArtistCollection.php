98cdb8d447c27e9077078ec3ac4d99603ec053e1
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2023-05-09 16:52:21 +0200

    Classe ArtistCollection.php

---
+++ src/Entity/Collection/ArtistCollection.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity\Collection;
+
+use Database\MyPdo;
+use Entity\Artist;
+use PDO;
+
+class ArtistCollection
+{
+    /**
+     * Méthode d'instance findAll
+     * Permet de récupérer dans un array
+     * tous les artistes de la class artiste
+     * de notre base de donnée
+     * @return Artist[]
+     */
+    public function findAll(): array
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+    SELECT id, name
+    FROM artist
+    ORDER BY name
+SQL
+        );
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_CLASS, Artist::class);
+        return $stmt->fetchAll();
+
+    }
+}
