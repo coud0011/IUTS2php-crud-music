f22fd9136de951f89f73b707e3794afd9fef2e30
Axel <axel.coudrot@etudiant.univ-reims.fr>
2023-05-15 12:08:34 +0200

    auto test album collection : écriture en cours

---
+++ tests/Crud/Collection/AlbumCollectionCest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Collection;
+
+use Tests\CrudTester;
+
+class AlbumCollectionCest
+{
+    public function _before(CrudTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(CrudTester $I)
+    {
+    }
+}
