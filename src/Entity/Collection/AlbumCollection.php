a528e4e87b6595196196541d3d376d09c9faab4d
Axel <axel.coudrot@etudiant.univ-reims.fr>
2023-05-15 12:07:56 +0200

    Class AlbumCollection écrite

---
+++ src/Entity/Collection/AlbumCollection.php
@@ -0,0 +1,37 @@
+<?php
+
+# File created by Axel Coudrot
+# Created with PhpStorm by JetBrains Studios
+# License for JetBrains : student Noncommercial
+
+declare(strict_types=1);
+
+namespace Entity\Collection;
+
+use Database\MyPdo;
+use Entity\Album;
+use PDO;
+
+class AlbumCollection
+{
+    /**
+     * Méthode d'instance findAll
+     * Permet de récupérer dans un array
+     * tous les artistes de la class artiste
+     * de notre base de donnée
+     * @return Album[]
+     */
+    public static function findAll(): array
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+    SELECT id, name, year, artistId, genreId, coverId
+    FROM album
+    ORDER BY year desc, name
+SQL
+        );
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_CLASS, Album::class);
+        return $stmt->fetchAll();
+    }
+}
