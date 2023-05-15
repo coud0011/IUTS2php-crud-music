04dab00d7f3fabf5f577f755895e06f1bceaf9e5
Axel <axel.coudrot@etudiant.univ-reims.fr>
2023-05-15 11:45:38 +0200

    Modification du fichier index.php : refactorisation en utilisant les classes Artist et ArtistCollection

--- public/index.php
+++ public/index.php
@@ -1,31 +1,31 @@
 <?php
 
 declare(strict_types=1);
-use Database\MyPdo;
+
 use Html\WebPage;
+use Entity\Collection\ArtistCollection;
 
 $webPage= new WebPage("Application de consultation et de modification d'une base de donnée de musique");
-$webPage->appendContent("<h1>Hello Music!</h1>");
-
-$stmt = MyPDO::getInstance()->prepare(
-    <<<'SQL'
-    SELECT id, name
-    FROM artist
-    ORDER BY name
-SQL
+$webPage->appendContent(
+    <<<HTML
+        <h1>Hello Music!</h1>
+        <ul>
+HTML
 );
 
-$stmt->execute();
-$webPage->appendContent("\n    <table>\n      <tr><th>Nom de l'artiste</th></tr>\n");
-while (($ligne = $stmt->fetch()) !== false) {
-    $id=$webPage->escapeString("{$ligne['id']}");
+$artistCollection=ArtistCollection::findAll();
+foreach($artistCollection as $key=>$artist) {
+    $id=$webPage->escapeString("{$artist->getId()}");
+    $name=$webPage->escapeString($artist->getName());
     $webPage->appendContent(
         <<<HTML
-      <td>
-        <td><a href="artist.php?artistId=$id">{$webPage->escapeString($ligne['name'])}</a></td>
-      </tr>
+        <li><a href="artist.php?artistId=$id">$name</a></li>
 HTML
     );
 }
-$webPage->appendContent("    </table>\n  </body>\n</html>");
+$webPage->appendContent(
+    <<<HTML
+    </ul>
+HTML
+);
 echo $webPage->toHTML();
