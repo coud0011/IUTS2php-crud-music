b2d19aa47676e1aeecbf57df9d4c2eadc736eee8
Axel <axel.coudrot@etudiant.univ-reims.fr>
2023-06-06 11:59:48 +0200

    Artistform départ

---
+++ src/Html/Form/ArtistForm.php
@@ -0,0 +1,13 @@
+<?php
+# File created by Axel Coudrot
+# Created with PhpStorm by JetBrains Studios
+# License for JetBrains : student Noncommercial
+
+declare(strict_types=1);
+
+namespace Html\Form;
+
+class ArtistForm
+{
+
+}
